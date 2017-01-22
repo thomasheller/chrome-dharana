980b0e4d57b3d326fa6e28ea84400173b89ae172
Thomas Heller <thomas.m.heller@gmail.com>
2017-01-22 15:01:06 +0100

    toggle and more elements for StackExchange

--- stackexchange.js
+++ stackexchange.js
@@ -1,6 +1,34 @@
-var jobs = $('#hireme');
-jobs.hide();
+var sidebar = $('#sidebar');
 
+var jobs = $('#hireme');
 var hot = $('#hot-network-questions');
+var chat = $('#chat-feature');
+var community = $('.community-bulletin');
+var newsletter = $('#newsletter-ad');
+
+var buttonShow = $('<p><button>show more sidebar content</button></p>');
+var buttonHide = $('<p><button>hide more sidebar content</button></p>');
+
+var toggleAction = function () {
+  buttonShow.toggle();
+  buttonHide.toggle();
+  jobs.toggle();
+  hot.toggle();
+  chat.toggle();
+  community.toggle();
+  newsletter.toggle();
+};
+
+buttonShow.click(toggleAction);
+buttonHide.click(toggleAction);
+
+sidebar.prepend(buttonShow);
+sidebar.prepend(buttonHide);
+
+jobs.hide();
 hot.hide();
+chat.hide();
+community.hide();
+newsletter.hide();
+buttonHide.hide();
 
